3644f7dca4849c7d8206adeda876c0f2906f65a0
Faisal Zaid <zaidalideveloper@gmail.com>
2025-09-26 17:39:10 +0300

    final commit

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    
+    return tutorial
+      .split(" ")
+      .map(word => word[0].toUpperCase() + word.slice(1))
+      .join(" ");
+  });
 }
+
+
+
+
+
